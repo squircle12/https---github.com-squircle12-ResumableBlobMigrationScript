e54cb94d582b53501c7d860e7e4a0ec6d644656f
squircle12 <leigh@gwyncon.com>
2026-02-12 15:06:18 +0000

    Remove README_V2_Approach.md as it is no longer relevant to the Blob Migration V2 project, streamlining documentation and eliminating outdated information.

---
+++ BU Import Scripts/YnysMon.sql
@@ -0,0 +1,14 @@
+
+('EF43C818-DA8C-E711-80DC-0050569C6AE2', 'Gwynedd - Ynys Mon - Integrated Social Care')
+, ('70F23C5A-DA8C-E711-80DC-0050569C6AE2', 'Gwynedd - Ynys Mon - Integrated Adults and Children Social Care')
+, ('8A20AF3A-DA8C-E711-80DC-0050569C6AE2', 'Gwynedd - Ynys Mon - Integrated Adults Social Care')
+, ('1B17EF48-DA8C-E711-80DC-0050569C6AE2', 'Gwynedd - Ynys Mon - Integrated Children Social Care')
+ 
+, ('3EBC30FD-FB80-E711-80D4-005056AD24B6', 'Ynys Môn - Social Care')
+ 
+, ('F028630A-FC80-E711-80D4-005056AD24B6', 'Ynys Mon - Adults')
+, ('4FD80713-FC80-E711-80D4-005056AD24B6', 'Ynys Mon - Children')
+ 
+, ('35A6011A-FC80-E711-80D4-005056AD24B6', 'Ynys Mon - Secure Adults')
+, ('347D6624-FC80-E711-80D4-005056AD24B6', 'Ynys Mon - Secure Children')
+ 
