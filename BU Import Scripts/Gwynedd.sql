da21c2b9b4170ee5d094c2572916061bfcc58214
squircle12 <leigh@gwyncon.com>
2026-02-25 16:44:22 +0000

    Refactor SQL scripts in BlobDeltaJobs to enhance error handling and improve logging mechanisms. Update stored procedures to ensure better tracking of execution status and streamline the overall process for improved reliability.

---
+++ BU Import Scripts/Gwynedd.sql
@@ -0,0 +1,11 @@
+INSERT INTO [dbo].[LA_BU] ([businessunit], [BU_Name], [Organisation])
+VALUES
+('EF43C818-DA8C-E711-80DC-0050569C6AE2', LEFT('Gwynedd - Ynys Môn - Integrated Social Care',100),'Gwynedd')
+, ('8A20AF3A-DA8C-E711-80DC-0050569C6AE2', LEFT('Gwynedd - Ynys Môn - Integrated Adults Social Care',100),'Gwynedd')
+, ('70F23C5A-DA8C-E711-80DC-0050569C6AE2', LEFT('Gwynedd - Ynys Môn - Integrated Adults and Children Social Care',100),'Gwynedd')
+, ('8D351EDD-A977-E711-80D3-005056AD65B4', LEFT('Gwynedd - Social Care',100),'Gwynedd')
+, ('3396BBE5-A977-E711-80D3-005056AD65B4', LEFT('Gwynedd - Adults',100),'Gwynedd')
+, ('25A026F1-A977-E711-80D3-005056AD65B4', LEFT('Gwynedd - Children',100),'Gwynedd')
+, ('C17F5502-AA77-E711-80D3-005056AD65B4', LEFT('Gwynedd - Secure Children',100),'Gwynedd')
+
+GO
